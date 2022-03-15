542fc5a9faec1aee0a3062249f25879c0c96a388
layash <mrtech.layash@gmail.com>
2022-03-15 16:42:56 +0545

    typo fixed in file name

---
+++ src/constants/socket.constants.js
@@ -0,0 +1,13 @@
+export const SOCKET_URL = "ws://localhost:8999";
+export const SOCKET_CLOSING_TIMEOUT = 30000;
+
+export const SERVER_CONNECTED_MESSAGE = "Server Connected";
+export const SERVER_DISCONNECTED_MESSAGE = "Server Disconnected";
+export const SERVER_CLOSED_MESSAGE = "Connection to Server is closed";
+export const SERVER_CONNECTION_ERROR_MESSAGE = "Connection to Server is failed";
+
+export const serverStatus = {
+  OPEN: "open",
+  CLOSE: "close",
+  ERROR: "error",
+};
