330ef96b0e9c8e3f9085c7646daa9e000601d30a
layash <mrtech.layash@gmail.com>
2022-03-16 16:00:18 +0545

    comments/feedback resolved

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import {
   SERVER_CONNECTION_ERROR_MESSAGE,
   SERVER_DISCONNECTED_MESSAGE,
   SOCKET_CLOSING_TIMEOUT,
+  SOCKET_CLOSING_TIMEOUT_MESSAGE,
   SOCKET_URL,
-} from "./constants/socket.constatns";
+} from "./constants/socket.constants";
 import {
   LineChart,
   Line,
@@ -23,25 +24,34 @@ import {
 
 function App() {
   const [data, setData] = useState([]);
+  const [dataTwo, setDataTwo] = useState([]);
   const [temperatureOne, setDataTemperatureOne] = useState([]);
   const [temperatureTwo, setDataTemperatureTwo] = useState([]);
 
   useEffect(() => {
-    const ws = new WebSocket(SOCKET_URL);
+    const ws = new WebSocket(SOCKET_URL, "ws");
+
     ws.onopen = (event) => {
+      console.log("open", event);
       if (event.type === serverStatus.OPEN) {
         toast.dismiss();
         toast.success(SERVER_CONNECTED_MESSAGE);
       }
     };
+
     ws.onclose = (event) => {
-      console.log(event)
-      if (event.type === serverStatus.CLOSE) {
-        toast.dismiss();
+      console.log("close", event);
+      toast.dismiss();
+
+      if (event.wasClean) {
+        toast.error(event.reason);
+      } else {
         toast.error(SERVER_DISCONNECTED_MESSAGE);
       }
     };
+
     ws.onerror = (event) => {
+      console.log("error", event);
       if (event.type === serverStatus.ERROR) {
         toast.dismiss();
         toast.error(SERVER_CONNECTION_ERROR_MESSAGE);
@@ -49,60 +59,84 @@ function App() {
     };
 
     ws.onmessage = (temperatureData) => {
-      const parsedData = JSON.parse(temperatureData.data);
       let graphData = {};
-
+      const parsedData = JSON.parse(temperatureData.data);
       parsedData.forEach((element) => {
-        if (element.data <= 100) {
-          graphData = {
-            timeStamp: new Date(parsedData[0].timestamp).getMinutes(),
-            temperature1: parsedData[0].temperature,
-            temperature2: parsedData[1].temperature,
-          };
-          setDataTemperatureOne(parsedData[0].temperature);
-          setDataTemperatureTwo(parsedData[1].temperature);
-          setData((currentData) => [...currentData, graphData]);
+        if (element.data < 100) {
+          graphData["dataValue"] = element.data;
+          graphData["timestamp"] = element.timestamp;
+
+          if (element.id === 1) {
+            graphData["temperature1"] = element.temperature;
+            setDataTemperatureOne(element.temperature);
+          } else {
+            graphData["temperature2"] = element.temperature;
+            setDataTemperatureTwo(element.temperature);
+          }
+          if ("temperature1" in graphData && "temperature2" in graphData) {
+            console.log(parsedData, graphData);
+            setData((currentData) => [...currentData, graphData]);
+          }
         }
       });
     };
 
     const closeWebsocket = () => {
-      ws.close();
+      console.log("called");
+      ws.close(1000, SOCKET_CLOSING_TIMEOUT_MESSAGE);
     };
 
-    setTimeout(closeWebsocket, SOCKET_CLOSING_TIMEOUT);
+    const timer = setTimeout(closeWebsocket, SOCKET_CLOSING_TIMEOUT);
 
+    return () => clearTimeout(timer);
   }, []);
-
-
-
 
   return (
     <>
       <div className="row">
         <div className="child-div">
-        <label><strong className='child-strong'>ID 1 </strong></label>
-         <div className="temperature one" >Temp {temperatureOne} c</div>
+          <label>
+            <strong className="child-strong">ID 1 </strong>
+          </label>
+
+          <div className="temperature one">Temp {temperatureOne} °C</div>
         </div>
         <div className="child-div">
-        <label><strong className='child-strong'>ID 2 </strong></label> 
-        <div className="temperature two">Temp {temperatureTwo} c</div>
+          <label>
+            <strong className="child-strong">ID 2 </strong>
+          </label>
+          <div className="temperature two">Temp {temperatureTwo} °C</div>
         </div>
       </div>
       <ResponsiveContainer width="100%" height={400}>
-        <LineChart width={400} height={200} data={data}>
+        <LineChart
+          width={400}
+          height={200}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="timeStamp" />
-          <YAxis />
+          <XAxis  type="number" />
+          <YAxis  datakey="dataValue" label={"Data"} />
           <Tooltip />
           <Legend />
           <Line
+            connectNulls
             type="linear"
             dataKey="temperature1"
             stroke="#8884d8"
-            activeDot={{ r: 8 }}
           />
-          <Line type="linear" dataKey="temperature2" stroke="#82ca9d" />
+          <Line
+            connectNulls
+            type="linear"
+            dataKey="temperature2"
+            stroke="#82ca9d"
+          />
         </LineChart>
       </ResponsiveContainer>
       <ToastContainer />
--- src/constants/socket.constants.js
+++ src/constants/socket.constants.js
@@ -1,10 +1,11 @@
 export const SOCKET_URL = "ws://localhost:8999";
-export const SOCKET_CLOSING_TIMEOUT = 30000;
+export const SOCKET_CLOSING_TIMEOUT = 15000;
 
 export const SERVER_CONNECTED_MESSAGE = "Server Connected";
 export const SERVER_DISCONNECTED_MESSAGE = "Server Disconnected";
 export const SERVER_CLOSED_MESSAGE = "Connection to Server is closed";
 export const SERVER_CONNECTION_ERROR_MESSAGE = "Connection to Server is failed";
+export const SOCKET_CLOSING_TIMEOUT_MESSAGE = "Connection to Server is closed due to time limit";
 
 export const serverStatus = {
   OPEN: "open",
